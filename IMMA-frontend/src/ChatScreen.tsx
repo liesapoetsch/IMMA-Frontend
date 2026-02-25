1627fa3376e413f4ca1914348b50a6f5c4405e6b
Liesa Pötsch <liesa.poetsch@boc-de.com>
2026-02-25 14:50:14 +0100

    ChatScreen added

---
+++ IMMA-frontend/src/ChatScreen.tsx
@@ -0,0 +1,16 @@
+function ChatScreen(){
+    const textInput = "Hallo"
+
+    return (
+        <>
+        TEST
+            <div className={"container"}>
+                <input type={"text"} name={"TextInput"} value={textInput}/>
+            </div>
+
+
+</>
+)
+}
+
+export default ChatScreen

23578f3488c28638c5e87cb23db72810b37b2188
Florimond <florimondgienow@outlook.de>
2026-03-01 14:59:24 +0100

    first api-connection functions

--- IMMA-frontend/src/App.tsx
+++ IMMA-frontend/src/App.tsx
@@ -1,18 +1,22 @@
 import { useEffect, useState } from 'react';
 
-interface Item {
-    id: number;
-    name: string;
-    description: string;
+interface Request {
+    gemini_request: string;
+    image: string;
 }
 
 function App() {
-    const [items, setItems] = useState<Item[]>([]);
+
+
+
+    const [requests, setRequests] = useState<Request[]>([{gemini_request:"asdasdsad", image: "text"}]);
+
+
 
     useEffect(() => {
-        fetch('http://localhost:8000/api/items/')
+        fetch('http://localhost:8000/api/text_output/', {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(requests[0])})
             .then(res => res.json())
-            .then(data => setItems(data))
+            .then(data => setRequests([data]))
             .catch(err => console.error('Oops!', err));
     }, []);
 
@@ -20,9 +24,9 @@ function App() {
         <div>
             <h1>Items List</h1>
             <ul>
-                {items.map(item => (
-                    <li key={item.id}>
-                        <strong>{item.name}</strong>: {item.description}
+                {requests.map(request => (
+                    <li key={request.gemini_request}>
+                        <strong>{request.image}</strong>: {request.image}
                     </li>
                 ))}
             </ul>
